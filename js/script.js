416dd60837a752380c11103fcb906862fce01b3a
Dan <kyznetsovdanila@gmail.com>
2016-09-06 22:04:43 +0300

    Последние

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var link = document.querySelector(".company-feedback-btn");
+   var link = document.querySelector(".company-feedback-btn");
 
 var popup = document.querySelector(".modal-content-write");
 
@@ -53,3 +53,25 @@ form.addEventListener("submit", function(event) {
 		}
 		}
 });
+        
+        var mapOpen = document.querySelector(".js-open-map");
+        var mapPopup = document.querySelector(".modal-content-map");
+        var mapClose = mapPopup.querySelector(".modal-content-close");
+
+      mapOpen.addEventListener("click", function(event) {
+        event.preventDefault();
+        mapPopup.classList.add("modal-content-show");
+      });
+
+      mapClose.addEventListener("click", function(event) {
+        event.preventDefault();
+        mapPopup.classList.remove("modal-content-show");
+      });
+
+      window.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+          if (mapPopup.classList.contains("modal-content-show")) {
+            mapPopup.classList.remove("modal-content-show");
+          }
+        }
+      });
